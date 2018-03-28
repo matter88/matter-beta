8c8d3cfa12807a76ae62467c1c0a1a050c597196
Hoang Nguyen <hnguyen31@gmail.com>
2018-03-27 23:34:25 -0400

    bootstrap needed to be installed in root directory of client, nav bar functioning

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header.jsx'
 import Main from './Main.jsx'
+import { Button } from 'react-bootstrap'
 
 const App = () => (
   <div>
     <Header />
+    <Button bsStyle="success">Success</Button>
     <Main />
   </div>
 )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-    <nav>
+    <Navbar>
         <span><Link to='/'>About</Link></span>
         <span><Link to='/ico'>ICO</Link></span>
         <span><Link to='/demo'>Demo</Link></span>
         <span><Link to='/whitepaper'>Whitepaper</Link></span>
         <span><Link to='/contact'>Contact</Link></span>
-    </nav>
-  </header>
+    </Navbar>
 )
 
 export default Header
